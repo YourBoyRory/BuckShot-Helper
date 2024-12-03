b07406af1c148d78998e243dc4cbae78b8ec3cd9
YourBoyRory <yourboyrory@gmail.com>
2024-12-02 22:42:25 -0500

    Fixed build scripts

--- build-windows.cmd
+++ build-windows.cmd
@@ -2,7 +2,7 @@
 python -m venv ./venv-windows
 source ./venv-windows/Scripts/activate.bat
 pip install -r requirements.txt
-pyinstaller --windowed --onefile --name "Buckshot Helper" ./BuckShotHelper.py
+pyinstaller --onefile --windowed --name "Buckshot Helper" ./BuckShotHelper.py
 echo.
 echo Build Done
 echo.
